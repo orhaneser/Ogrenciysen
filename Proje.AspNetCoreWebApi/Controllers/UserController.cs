8829f815b8556b1f751330ee2712ba32ec2163ba
unknown <samet.oz17@gmail.com>
2019-05-14 23:10:02 +0300

    Controller Update

---
+++ Proje.AspNetCoreWebApi/Controllers/UserController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Proje.Business.Logic;
+using Proje.DataAccess.Context;
+using Proje.Entity.Model;
+using Proje.Interface;
+using Newtonsoft.Json;
+using Proje.Business.Helper;
+using Newtonsoft.Json.Linq;
+
+namespace Proje.AspNetCoreWebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        #region service
+        private readonly IGenericService<Password> passwordService;
+        private readonly IGenericService<User> UserService;
+        private readonly IGenericService<Role> roleService;
+        public UserController(IGenericService<User> userService, IGenericService<Role> roleservice, IGenericService<Password> passwordservice)
+        {
+            passwordService = passwordservice;
+            UserService = userService;
+            roleService = roleservice;
+        }
+        #endregion
+        [HttpGet]
+        [Route("~/User/GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = (from u in UserService.GetAll()
+                          join r in roleService.GetAll()
+                          on u.UserID equals r.UserID
+                          join p in passwordService.GetAll()
+                          on u.UserID equals p.UserID
+                          select new
+                          {
+                              id = u.UserID,
+                              name = u.Name,
+                              surname = u.Surname,
+                              token = u.Token,
+                              gender = u.Gender,
+                              roleid = r.RoleID,
+                              roletext = r.RoleText,
+                              password = p.Pass
+                          }).ToList();
+
+            var json = JsonConvert.SerializeObject(result);
+            return Ok(json);
+        }
+        [HttpGet]
+        [Route("~/User/Get/{id}")]
+        public IActionResult Get(int id)
+        {
+            var item = UserService.Get(id);
+            var result = (from u in UserService.GetAll()
+                          join r in roleService.GetAll()
+                          on item.UserID equals r.UserID
+                          join p in passwordService.GetAll()
+                          on item.UserID equals p.UserID
+                          select new
+                          {
+                              id = item.UserID,
+                              name = item.Name,
+                              surname = item.Surname,
+                              token = item.Token,
+                              gender = item.Gender,
+                              roleid = r.RoleID,
+                              roletext = r.RoleText,
+                              password = p.Pass
+                          }).ToList().FirstOrDefault();
+
+            var json = JsonConvert.SerializeObject(result);
+            return Ok(json);
+        }
+        [HttpDelete]
+        [Route("~/User/Delete/{id}")]
+        public ResultHelper Delete(int id)
+        {
+            User user = UserService.Get(id);
+            if (user == null)
+            {
+                return new ResultHelper(true, user.UserID, ResultHelper.UnSuccessMessage);
+            }
+
+            UserService.Delete(user);
+            return new ResultHelper(true, user.UserID, ResultHelper.SuccessMessage);
+        }
+        [HttpPut]
+        [Route("~/User/Update/{id}")]
+        public ResultHelper Put(int id, [FromBody] User user)
+        {
+            if (user == null)
+            {
+                return new ResultHelper(true, user.UserID, ResultHelper.UnSuccessMessage);
+            }
+
+            UserService.Set(user);
+            return new ResultHelper(true, user.UserID, ResultHelper.SuccessMessage);
+
+        }
+        [HttpPost]
+        [Route("~/User/Add")]
+        public ResultHelper Post([FromBody] JObject data)
+        {
+            User user = data["UserData"].ToObject<User>();
+            Role role = data["RoleData"].ToObject<Role>();
+            var item = UserService.Create(user);
+            role.UserID = item.UserID;
+
+            roleService.Create(role);
+            return new ResultHelper(true, user.UserID, ResultHelper.SuccessMessage);
+
+        }
+
+    }
+}
---
+++ Proje.AspNetCoreWebApi/Startup.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Proje.Business.Logic;
+using Proje.DataAccess.Context;
+using Proje.Entity.Model;
+using Proje.Interface;
+
+namespace Proje.AspNetCoreWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=Proje;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<ProjeContext>(options => options.UseSqlServer(connection));
+            services.AddScoped(p => new ProjeContext(p.GetService<DbContextOptions<ProjeContext>>()));
+            services.AddScoped<IGenericService<Advert>,AdvertLogic>();
+            services.AddScoped<IGenericService<AdvertStatus>, AdvertStatusLogic>();
+            services.AddScoped<IGenericService<Avatar>, AvatarLogic>();
+            services.AddScoped<IGenericService<Category>, CategoryLogic>();
+            services.AddScoped<IGenericService<Email>, EmailLogic>();
+            services.AddScoped<IGenericService<Message>, MessageLogic>();
+            services.AddScoped<IGenericService<OldPassword>, OldPasswordLogic>();
+            services.AddScoped<IGenericService<Password>, PasswordLogic>();
+            services.AddScoped<IGenericService<Phone>, PhoneLogic>();
+            services.AddScoped<IGenericService<Photo>, PhotoLogic>();
+            services.AddScoped<IGenericService<RegisterTemp>, RegisterTempLogic>();
+            services.AddScoped<IGenericService<Role>, RoleLogic>();
+            services.AddScoped<IGenericService<SharedPhoto>, SharedPhotoLogic>();
+            services.AddScoped<IGenericService<UniCity>, UniCityLogic>();
+            services.AddScoped<IGenericService<Univercity>, UnivercityLogic>();
+            services.AddScoped<IGenericService<UserLocation>, UserLocationLogic>();
+            services.AddScoped<IGenericService<User>, UserLogic>();
+            services.AddScoped<IGenericService<UserPassword>, UserPasswordLogic>();
+            services.AddScoped<IGenericService<UserStarAndComment>, UserStarAndCommentLogic>();
+            services.AddScoped<IGenericService<UserTime>, UserTimeLogic>();
+            services.AddScoped<IGenericService<AdvertSeenUser>, AdvertSeenUserLogic>();
+            services.AddScoped<IGenericService<Notification>, NotificationLogic>();
+            services.AddScoped<IGenericService<NotificationType>, NotificationTypeLogic>();
+            services.AddScoped<IGenericService<FavAdvert>, FavAdvertLogic>();
+            services.AddScoped<IGenericService<Complaint>, ComplaintLogic>();
+            services.AddScoped<IGenericService<BlockedUser>, BlockedUserLogic>();
+
+            // BloggingContext requires
+            // using EFGetStarted.AspNetCore.NewDb.Models;
+            // UseSqlServer requires
+            // using Microsoft.EntityFrameworkCore;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
